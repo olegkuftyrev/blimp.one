1c2c88258e0fe7e29a4bdc9a9fb2f3529af83b86
Oleg Kuftyrev <rayn.winter@gmail.com>
2025-06-03 22:22:40 -0700

    action step using navigate vs href

--- src/components/ActionSecret.jsx
+++ src/components/ActionSecret.jsx
@@ -1,15 +1,15 @@
-// src/components/ActionSecret.jsx
 import { ActionBar, Portal, Input, Button, HStack } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ActionSecret() {
   const [value, setValue] = useState("");
-
   const isValid = value === "2475";
+  const navigate = useNavigate(); // импортируем хук
 
   const handleGo = () => {
     if (isValid) {
-      window.location.href = "/secret";
+      navigate("/secret"); // вот это по-реактовски!
     }
   };
 
